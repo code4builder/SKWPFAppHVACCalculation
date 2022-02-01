e03f0dbf84e2dce7816aa3cf48e30fcab99ee83e
Sergey Kuleshov <code4builder@gmail.com>
2022-02-01 06:19:07 +0100

    template of menu and rect duct calculation done

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -23,6 +23,74 @@ namespace SKUWPFAppHVAC
         public MainWindow()
         {
             InitializeComponent();
+
         }
+        int rectDuctWidth, rectDuctHeight, rectDuctAirFlow;
+        double rectDuctVelocity, rectDuctAirFlowLS;
+
+        void CalculateRectDuctVelocity()
+        {
+            double equivalentDiameterRectDuct = 1.3 * (Math.Pow((rectDuctWidth * rectDuctHeight), 0.625) / Math.Pow((rectDuctWidth + rectDuctHeight), 0.25));
+            double sectionAreaEquivDiam = 3.14 * Math.Pow((equivalentDiameterRectDuct / 1000), 2) / 4;
+            rectDuctVelocity = Math.Round((rectDuctAirFlow / (sectionAreaEquivDiam * 3600)), 2);
+            rectDuctAirFlowLS = Math.Round((rectDuctAirFlow / 3.6),2);
+
+            boxRectDuctAirFlowLS.Text = rectDuctAirFlowLS.ToString();
+            boxRectVelocity.Text = rectDuctVelocity.ToString();
+        }
+
+        private void MIAbout_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("This application was created by Sergey Kulkeshov/n Email:");
+        }
+
+        private void BoxRectDuctWidth_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (boxRectDuctWidth != null && boxRectDuctHeight != null && boxRectDuctAirFlow != null && boxRectVelocity != null && boxRectDuctAirFlowLS != null)
+            {
+            try
+            {
+                rectDuctWidth = Int32.Parse(boxRectDuctWidth.Text.Trim());
+
+            }
+            catch (Exception myException) { MessageBox.Show("You can type only numbers: " + myException.Message); }
+
+                CalculateRectDuctVelocity();
+            }
+        }
+        private void BoxRectDuctHeight_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (boxRectDuctWidth != null && boxRectDuctHeight != null && boxRectDuctAirFlow != null && boxRectVelocity != null && boxRectDuctAirFlowLS != null)
+            {
+                try
+                {
+                    rectDuctHeight = Int32.Parse(boxRectDuctHeight.Text.Trim());
+                }
+                catch (Exception myException) { MessageBox.Show("You can type only numbers: " + myException.Message); }
+
+                CalculateRectDuctVelocity();
+            }
+        }
+        private void BoxRectDuctAirFlow_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (boxRectDuctWidth != null && boxRectDuctHeight != null && boxRectDuctAirFlow != null && boxRectVelocity != null && boxRectDuctAirFlowLS != null)
+            {
+                try
+                {
+
+                    rectDuctAirFlow = Int32.Parse(boxRectDuctAirFlow.Text.Trim());
+                }
+                catch (Exception myException) { MessageBox.Show("You can type only numbers: " + myException.Message); }
+
+                CalculateRectDuctVelocity();
+            }
+        }
+
+
+
+
+
+
+
     }
 }
